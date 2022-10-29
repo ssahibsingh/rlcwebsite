910ea48df3db2f770b03905358d703423a130143
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2022-10-29 10:28:31 +0530

    Updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,45 +4,76 @@ import "./style.css";
 const AboutPage = () => {
   return (
     <>
-      <div className="aboutPage minHeight">
+      <div className="aboutPage">
         <div className="container">
           <h1 className="my-3 text-center py-3 headColor">About Us</h1>
           <div className="content">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-              veniam amet non praesentium, optio iusto iste voluptate, incidunt,
-              ratione unde illum fugit. Doloremque natus obcaecati hic fuga
-              consectetur incidunt aperiam. Doloribus culpa temporibus debitis
-              veniam cupiditate ullam voluptates autem id tempora. Nemo itaque
-              aspernatur ducimus odio ipsum sit commodi perspiciatis voluptatum
-              deleniti dolore nobis laborum corporis dolor magni inventore
-              natus, dolorem tempora ad exercitationem, iste similique in error
-              unde nisi. Assumenda, quam exercitationem quod eligendi debitis
-              magni inventore. Itaque laudantium labore, dolore ipsa dignissimos
-              asperiores. Unde qui eligendi quo saepe, illum odit autem quia
-              assumenda neque cupiditate laboriosam ut cumque, voluptates
-              quisquam delectus? Sunt eius illum sit harum ullam corrupti non
-              quae quo amet ipsam officiis aliquid omnis dolorem, ea cum ducimus
-              earum. Rerum error excepturi odio. Qui obcaecati eum ab
-              reprehenderit, delectus cumque minus corrupti vero nulla, mollitia
-              est velit eaque nemo iusto. Sit eaque quisquam atque doloremque
-              vel vitae excepturi enim placeat reiciendis eius!
-            </p>
+            <div className="px-4">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+                veniam amet non praesentium, optio iusto iste voluptate,
+                incidunt, ratione unde illum fugit. Doloremque natus obcaecati
+                hic fuga consectetur incidunt aperiam. Doloribus culpa
+                temporibus debitis veniam cupiditate ullam voluptates autem id
+                tempora. Nemo itaque aspernatur ducimus odio ipsum sit commodi
+                perspiciatis voluptatum deleniti dolore nobis laborum corporis
+                dolor magni inventore natus, dolorem tempora ad exercitationem,
+                iste similique in error unde nisi. Assumenda, quam
+                exercitationem quod eligendi debitis magni inventore. Itaque
+                laudantium labore, dolore ipsa dignissimos asperiores. Unde qui
+                eligendi quo saepe, illum odit autem quia assumenda neque
+                cupiditate laboriosam ut cumque, voluptates quisquam delectus?
+                Sunt eius illum sit harum ullam corrupti non quae quo amet ipsam
+                officiis aliquid omnis dolorem, ea cum ducimus earum. Rerum
+                error excepturi odio. Qui obcaecati eum ab reprehenderit,
+                delectus cumque minus corrupti vero nulla, mollitia est velit
+                eaque nemo iusto. Sit eaque quisquam atque doloremque vel vitae
+                excepturi enim placeat reiciendis eius!
+              </p>
+            </div>
 
-            <h2 className="headColor text-center">Patron and Faculy Coordinator</h2>
-            <div className="row">
-              <div className="col-md-6 d-flex justify-content-center">
-                <div>
-                  <img src="" alt="" />
-                  <h3>Dr Binod Kumar Kanaujia</h3>
-                  <h4>Director, NITJ</h4>
+            <div className="motive my-5 px-4 text-center">
+              <h2 className="text-center headColor">Motive</h2>
+              <p>To Promote and Preserve Regional Literature of India</p>
+            </div>
+            <div className="motive my-5 px-4 text-center">
+              <h2 className="text-center headColor">Activities</h2>
+              <ul className="list-unstyled">
+                <li>Book Club</li>
+                <li>Monthly Magazine</li>
+                <li>Talks with Prominent Personalities</li>
+                <li>Publishing Writings from NITJ</li>
+                <li>Competitions</li>
+                <li>Theatrical Acts</li>
+              </ul>
+            </div>
+
+            <div className="my-4">
+              <h2 className="headColor text-center">
+                Patron and Faculy Coordinator
+              </h2>
+              <div className="row mt-5">
+                <div className="col-md-6 d-flex justify-content-center text-center">
+                  <div>
+                    <img src="https://picsum.photos/250/300" alt="" />
+                    <h4 className="display-5 name fs-4 pt-2">
+                      Dr Binod Kumar Kanaujia
+                    </h4>
+                    <h5 className="display-6 designation fs-5">
+                      Director, NITJ
+                    </h5>
+                  </div>
                 </div>
-              </div>
-              <div className="col-md-6 d-flex justify-content-center">
-                <div>
-                  <img src="" alt="" />
-                  <h3>Dr Jagwinder Singh</h3>
-                  <h4>Director, NITJ</h4>
+                <div className="col-md-6 d-flex justify-content-center text-center">
+                  <div>
+                    <img src="https://picsum.photos/250/300" alt="" />
+                    <h4 className="display-5 name fs-4 pt-2">
+                      Dr Jagwinder Singh
+                    </h4>
+                    <h5 className="display-6 designation fs-5">
+                      Associate Professor & HOD H&M
+                    </h5>
+                  </div>
                 </div>
               </div>
             </div>
--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,13 +1,118 @@
-import React from 'react'
-import "./style.css"
+import React from "react";
+import "./style.css";
 const Events = () => {
   return (
     <>
       <div className="eventPage minHeight">
-        
+        <div className="header">
+          <div className="bgImg"></div>
+          <h1 className="my-3 text-center py-3 headColor heading d-flex justify-content-center align-items-center">
+            Events
+          </h1>
+        </div>
+        <div className="content">
+          <div className="my-5">
+            <div className="container">
+              <div className="row d-flex justify-content-center text-center">
+                <div
+                  className="col-md-4 card border-0 shadow-sm bg-light"
+                  style={{ width: "18rem" }}
+                >
+                  <div>
+                    <img
+                      src="https://picsum.photos/300/300"
+                      className="card-img-top"
+                      alt="..."
+                    />
+                    <div className="card-body">
+                      <span className="badge text-bg-danger">Upcoming</span>
+                      <h3 className="display-6 fs-5 fw-bold card-text">
+                        Some Event
+                      </h3>
+                      <p className="eventDescription">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quis ut soluta suscipit qui.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="my-5 pt-5">
+            <div className="container">
+              <div className="row d-flex justify-content-center text-center">
+                <div
+                  className="col-md-4 card border-0 shadow-sm bg-light mx-5 my-3"
+                  style={{ width: "18rem" }}
+                >
+                  <div>
+                    <img
+                      src="https://picsum.photos/300/300"
+                      className="card-img-top"
+                      alt="..."
+                    />
+                    <div className="card-body">
+                      <h3 className="display-6 fs-5 fw-bold card-text">
+                        Some Event
+                      </h3>
+                      <p className="eventDescription">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quis ut soluta suscipit qui.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className="col-md-4 card border-0 shadow-sm bg-light mx-5 my-3"
+                  style={{ width: "18rem" }}
+                >
+                  <div>
+                    <img
+                      src="https://picsum.photos/300/300"
+                      className="card-img-top"
+                      alt="..."
+                    />
+                    <div className="card-body">
+                      <h3 className="display-6 fs-5 fw-bold card-text">
+                        Some Event
+                      </h3>
+                      <p className="eventDescription">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quis ut soluta suscipit qui.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className="col-md-4 card border-0 shadow-sm bg-light mx-5 my-3"
+                  style={{ width: "18rem" }}
+                >
+                  <div>
+                    <img
+                      src="https://picsum.photos/300/300"
+                      className="card-img-top"
+                      alt="..."
+                    />
+                    <div className="card-body">
+                      <h3 className="display-6 fs-5 fw-bold card-text">
+                        Some Event
+                      </h3>
+                      <p className="eventDescription">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quis ut soluta suscipit qui.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Events
+export default Events;
